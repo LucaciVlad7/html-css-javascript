bc557134b7202b83b8c132b6438cf1f1cd3caddb
Lucaci Vlad-Andrei <lucacivladi@gmail.com>
2025-09-13 17:00:18 +0300

    Add files via upload

---
+++ React Project/src/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./components/App";
+import AddIcon from "@mui/icons-material/Add";
+import { Fab } from "@mui/material";
+import { Zoom } from "@mui/material";
+
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
